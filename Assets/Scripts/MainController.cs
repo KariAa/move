27c883707fc116ba99cc854f7262606058645604
KariAa <Kari.aaltonen@metropolia.fi>
2017-09-21 19:41:32 +0300

    initial files

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	PointerController up;
+	PointerController down;
+	PointerController left;
+	PointerController right;
+	//	RawImage box;
+	Ball ball;
+
+	// Use this for initialization, GetComponent<PointerController>
+	void Start () {
+		up = GameObject.Find ("Up").GetComponent<PointerController> ();
+		down = GameObject.Find ("Down").GetComponent<PointerController> ();
+		left = GameObject.Find ("Left").GetComponent<PointerController> ();
+		right = GameObject.Find ("Right").GetComponent<PointerController> ();
+		ball = GameObject.Find ("Ball").GetComponent<Ball>();
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (up.getPressed()) {
+			ball.transform.Translate (0,0.1f,0);	
+			//Debug.Log ("YLÖS painettu");
+		}
+		if (down.getPressed()) {
+			ball.transform.Translate (0,-0.1f,0);	
+			//Debug.Log ("ALAS painettu");
+		}
+		if (left.getPressed()) {
+			ball.transform.Translate (-0.1f,0,0);	
+			//Debug.Log ("LEFT painettu");
+		}
+		if (right.getPressed()) {
+			ball.transform.Translate (0.1f,0,0);
+			//Debug.Log ("RIGHT painettu");
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	//class PointerController extends MonoBehaviour implements IPointerDownHandler, IPointerUpHandler
+
+	bool pressed = false;
+
+
+	public void OnPointerDown (PointerEventData eventData) {
+		//Debug.Log ("Down - " + eventData);
+		pressed = true;
+	}
+
+	public void OnPointerUp (PointerEventData eventData) {
+		//Debug.Log ("Up " + eventData);
+		pressed = false;
+	}
+
+	public bool getPressed() {
+		return this.pressed;
+	}
+
+}
